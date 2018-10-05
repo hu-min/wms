2d25c82d14f7005ce45c7f13dd6932e8b6c074ff
vition <369709991@qq.com>
2018-10-05 16:41:35 +0800

    '导入导出'

--- Application/Admin/Controller/BaseController.class.php
+++ Application/Admin/Controller/BaseController.class.php
@@ -75,7 +75,7 @@ class BaseController extends \Common\Controller\BaseController{
             $this->assign('statusType',$this->statusType);
             $this->assign('statusTypeJ',json_encode($this->statusType));
             $this->assign('statusLabel',$this->statusLabel);
-            $this->assign('entries',[2,5,8,30,35,40,45,50]);
+            $this->assign('entries',[30,35,40,45,50]);
             // $nodeId = getTabId(I("vtabId"));
             $this->nodeId = getTabId(I("vtabId"));
             // $this->assign('processType',$this->processType);
