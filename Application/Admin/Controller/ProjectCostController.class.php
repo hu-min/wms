3685e06ebb181cd1e8e52bc0e2d1d9ba52a2eea4
vition <369709991@qq.com>
2019-04-17 06:42:57 +0800

    报价和成本增加了一个查看借支里的审批流

--- Application/Admin/Controller/ProjectCostController.class.php
+++ Application/Admin/Controller/ProjectCostController.class.php
@@ -436,9 +436,9 @@ class ProjectCostController extends BaseController{
             
             $offerCostCom = $this->pCostCom;
             $joins2 = [
+                "RIGHT JOIN (SELECT project_id e_project_id , section e_section FROM v_debit WHERE FIND_IN_SET({$roleId},examine)) e ON e.e_project_id = project_id AND e.e_section = section",
                 "LEFT JOIN (SELECT project_id o_project_id , section o_section,flag o_flag,total,actual_money,tax_rate,user_id ouser_id FROM v_project_offer ) o ON o.o_project_id = project_id AND o.o_section = section ",
                 "LEFT JOIN (SELECT userId, userName ouser_name FROM v_user ) ou ON ou.userId = ouser_id ",
-                "LEFT JOIN (SELECT project_id e_project_id , section e_section FROM v_debit WHERE FIND_IN_SET({$roleId},examine)) e ON e.e_project_id = project_id AND e.e_section = section",
             ];
             $listTemplate = 'project_costList';
             // $joins
@@ -481,7 +481,7 @@ class ProjectCostController extends BaseController{
         ];
         // $this->log($parameter);exit;
         $listResult=$offerCostCom->getList($parameter);
-        // echo $this->pCostCom->M()->_sql();exit;
+        echo $offerCostCom->_sql();exit;
         // $this->
         // if($type == 'offer'){
         //     $listTemplate = 'project_offerList';
